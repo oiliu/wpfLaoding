ec1aefd2dd381c6275d6b7b489d811f245fa18f3
oiliu <755422922@qq.com>
2016-02-18 15:53:50 +0800

    wpfLoading动画

---
+++ WpfApplication1/UserControl1.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        #region Data
+        private readonly DispatcherTimer animationTimer;
+        #endregion
+
+        public UserControl1()
+        {
+            InitializeComponent();
+
+            animationTimer = new DispatcherTimer(DispatcherPriority.ContextIdle, Dispatcher);
+            animationTimer.Interval = new TimeSpan(0, 0, 0, 0, 90);
+        }
+        #region Private Methods
+        private void Start()
+        {
+            animationTimer.Tick += HandleAnimationTick;
+            animationTimer.Start();
+        }
+
+        private void Stop()
+        {
+            animationTimer.Stop();
+            animationTimer.Tick -= HandleAnimationTick;
+        }
+
+        private void HandleAnimationTick(object sender, EventArgs e)
+        {
+            SpinnerRotate.Angle = (SpinnerRotate.Angle + 36) % 360;
+        }
+
+        private void HandleLoaded(object sender, RoutedEventArgs e)
+        {
+            const double offset = Math.PI;
+            const double step = Math.PI * 2 / 10.0;
+
+            SetPosition(C0, offset, 0.0, step);
+            SetPosition(C1, offset, 1.0, step);
+            SetPosition(C2, offset, 2.0, step);
+            SetPosition(C3, offset, 3.0, step);
+            SetPosition(C4, offset, 4.0, step);
+            SetPosition(C5, offset, 5.0, step);
+            SetPosition(C6, offset, 6.0, step);
+            SetPosition(C7, offset, 7.0, step);
+            SetPosition(C8, offset, 8.0, step);
+        }
+
+        private void SetPosition(Ellipse ellipse, double offset,
+            double posOffSet, double step)
+        {
+            ellipse.SetValue(Canvas.LeftProperty, 50.0 + Math.Sin(offset + posOffSet * step) * 50.0);
+
+            ellipse.SetValue(Canvas.TopProperty, 50 + Math.Cos(offset + posOffSet * step) * 50.0);
+        }
+
+        private void HandleUnloaded(object sender, RoutedEventArgs e)
+        {
+            Stop();
+        }
+
+        private void HandleVisibleChanged(object sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            bool isVisible = (bool)e.NewValue;
+
+            if (isVisible)
+                Start();
+            else
+                Stop();
+        }
+        #endregion
+
+        bool IsStart = false;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsStart)
+            {
+                Stop();
+                IsStart = false;
+                this.BtnButton.Content = "开始";
+            }
+            else
+            {
+                Start();
+                IsStart = true;
+                this.BtnButton.Content = "结束";
+            }
+        }
+
+        public void SetColor(Color c)
+        {
+            //Style s = this.Resources[myListBox] as Style;
+            SolidColorBrush s = this.Resources["CirclesColor"] as SolidColorBrush;
+            s.Color = c;
+            if (!IsStart)
+            {
+                Start();
+                IsStart = true;
+                this.BtnButton.Content = "结束";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Color color1 = 
+                (Color)System.Windows.Media.ColorConverter.ConvertFromString(btn.Background.ToString());
+            SetColor(color1);
+        }
+    }
+}
